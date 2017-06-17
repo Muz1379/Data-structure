0bdf8a96c469abaefa9357a7c9f663cf1c741565
学生李令新 <stullx@qq.com>
2017-06-17 23:22:39 +0800

    函数声明头文件

---
+++ QQ群重复成员查找/QQGroupsRMS/Function.h
@@ -0,0 +1,8 @@
+#pragma once
+#pragma execution_character_set("utf-8")
+//函数声明头文件
+
+char * fileread( char * );
+int contexec( char * , struct LISTNODE * );
+int isChinese( char * );
+int kmp( char *, char *, long );
