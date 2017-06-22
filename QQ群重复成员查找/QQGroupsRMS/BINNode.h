f6e9247be73cef906b39ccc1ca5a393f08e443ed
学生李令新 <stullx@qq.com>
2017-06-23 00:50:00 +0800

    更改一些注释

--- QQ群重复成员查找/QQGroupsRMS/BINNode.h
+++ QQ群重复成员查找/QQGroupsRMS/BINNode.h
@@ -4,6 +4,6 @@
 typedef struct BINTREE
 {
 	struct MEMBERNODE * member;//QQ成员信息结构体
-	struct BINTREE * left;	   //QQ号小值
-	struct BINTREE * right;	   //QQ号大值
+	struct BINTREE * left;	   //小值在左
+	struct BINTREE * right;	   //大值在右
 }binNODE;
