82ab2ffbb5ae21734c16f2796d17192653ec6e70
学生李令新 <stullx@qq.com>
2017-06-24 17:20:48 +0800

    完善主函数

--- QQ群重复成员查找/QQGroupsRMS/rms.c
+++ QQ群重复成员查找/QQGroupsRMS/rms.c
@@ -5,9 +5,13 @@
 #include <conio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <Windows.h>
-                                            //数据结构
 
+#define OK 1
+#define ERR -1
+#define NULL 0
+#define HASHLENGTH 500
+
+											//数据结构
 #include "BINNode.h"
 #include "MemberNode.h"
 #include "resNode.h"
@@ -18,21 +22,29 @@
 #include "ContExec.h"
 #include "isChinese.h"
 #include "KMP.h"
+#include "insHash.h"
 #include "SearchHash.h"
-#include "InsertList.h"
+#include "InsResList.h"
+#include "insBinTree.h"
+#include "LRDVisitBinTree.h"
+#include "HashCmp.h"
+#include "DLRSearchBinTree.h"
 
-#define OK 1
-#define ERR -1
-#define NULL 0
+//TODO:测试用
+#define fileNameA "testA.txt"
+#define fileNameB "testB.txt"
 
 int main( void )
 {
-	char fileNameA[100];	                //文件名A
-	char fileNameB[100];	                //文件名B
+	//TODO:测试用
+	/*
+	//char fileNameA[100];	                //文件名A
+	//char fileNameB[100];	                //文件名B
+	*/
 	char * contA;			                //内容字符串A
 	char * contB;			                //内容字符串B
-	struct BINTREE * hashA[500] = { NULL };	//设定哈希表长500，由于目前qq群最大人数为2000，哈希冲突使用拉链法解决。
-	struct BINTREE * hashB[500] = { NULL };
+	struct BINTREE * hashA[HASHLENGTH] = { NULL };	//设定哈希表长500，由于目前qq群最大人数为2000，哈希冲突使用拉链法解决。
+	struct BINTREE * hashB[HASHLENGTH] = { NULL };
 	struct BINTREE * tempA = { NULL };
 	struct BINTREE * tempB = { NULL };
 	struct RESNODE * resList = { NULL };
@@ -49,31 +61,35 @@ int main( void )
 	printf( "\n" );
 
 	                                        //TODO:此处打开文件生成字符串，应添加异常处理模块
+	//TODO:测试注释
+	/*
 	printf( "请输入QQ群【A】成员名单文件名：" );
 	scanf( "%s", fileNameA );
 	contA = fileread( fileNameA );
 	printf( "请输入QQ群【B】成员名单文件名：" );
 	scanf( "%s", fileNameB );
 	contB = fileread( fileNameB );
+	*/
+	contA = fileread( fileNameA );
+	contB = fileread( fileNameB );
 
 	contexec( contA ,hashA);
 	contexec( contB ,hashB);
 
-
-	hashcmp( hashA, hashB, resList );
 	//查找哈希表，生成结果链表
+	hashcmp( hashA, hashB, resList );
 
 	printf( "查找完毕，结果存储在resList单链表中" );
 	
-
-	if(resList != NULL)
+	resTemp = resList;
+	if( resTemp->next!= NULL)
 	{
 		printf( "%20s\t%50s\t%50s\t%50s\t%50s\t","QQ号", "A群名称", "A群内名片", "B群名称", "B群内名片" );
-		while(resList != NULL)
+		while(resTemp != NULL)
 		{
 			printf( "\n" );
-			printf( "%20s\t%50s\t%50s\t%50s\t%50s\t", resList->A->qqNum, resList->A->qqGroup, resList->A->personCard, resList->B->qqGroup, resList->B->personCard );
-			resList = resList->next;
+			printf( "%20s\t%50s\t%50s\t%50s\t%50s\t", resTemp->A->qqNum, resTemp->A->qqGroup, resTemp->A->personCard, resTemp->B->qqGroup, resTemp->B->personCard );
+			resTemp = resTemp->next;
 		}
 	}
 	else
