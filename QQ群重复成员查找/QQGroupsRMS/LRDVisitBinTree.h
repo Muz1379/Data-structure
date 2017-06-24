44589c40a34c0183bd725ac1b67ade445c94919f
学生李令新 <stullx@qq.com>
2017-06-24 17:20:06 +0800

    后序遍历二叉树

---
+++ QQ群重复成员查找/QQGroupsRMS/LRDVisitBinTree.h
@@ -0,0 +1,23 @@
+#pragma once
+#pragma execution_character_set("utf-8")
+
+#define OK 1
+#define ERR -1
+#define NULL 0
+
+#include "BINNode.h"
+#include "SearchHash.h"
+
+int LRDvisitbintree( struct BINTREE * bintree, struct BINTREE *hashB[], struct RESNODE * resList )
+{
+	if(bintree->left != NULL)//遍历左子树
+	{
+		LRDvisitbintree( bintree->left, hashB, resList );
+	}
+	if(bintree->right != NULL)//遍历右子树
+	{
+		LRDvisitbintree( bintree->right, hashB, resList );
+	}
+	searchhash( bintree, hashB, resList );
+	return OK;
+}
