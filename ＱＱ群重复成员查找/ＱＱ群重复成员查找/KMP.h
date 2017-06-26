341ded7ac08eeaaecb0cb754e0357ea691240536
学生李令新 <stullx@qq.com>
2017-06-26 12:23:28 +0800

    修复kmp匹配

--- ＱＱ群重复成员查找/ＱＱ群重复成员查找/KMP.h
+++ ＱＱ群重复成员查找/ＱＱ群重复成员查找/KMP.h
@@ -1,76 +1,73 @@
 #pragma once
-
-long kmp( unsigned char * master, unsigned char * pattern, long start )
-{
-	int j = 0;
-	long i = start;
-	int t = strlen( pattern );
-	long L = strlen( master );
-
-	for(; i < L&&j < t; i++)
-	{
-		if(*(master + i) == *(pattern + j))
-		{
-			j++;
-		}
-		else
-		{
-			j = 0;
-		}
-	}
-	return i-start;
-}
-//TODO:此处kmp算法实现不成功
-//void getnext( char p[], int next[] )
-//{
-//	int k = -1;
-//	int j = 0;
-//
-//	next[j] = k;
-//
-//	while(j < strlen( p ))
-//	{
-//		if((k == -1) || (p[j] == p[k])) //注意等号是==，而不是=  
-//		{
-//			++k; // 注意是先加后使用  
-//			++j;
-//			next[j] = k;
-//		}
-//		else
-//		{
-//			k = next[k];
-//		}
-//	}
-//
-//}
-//
-//long kmp( char * master, char * pattern, long i )
+//TODO:此处暴力匹配成功。
+//long kmp( unsigned char * master, unsigned char * pattern, long start )
 //{
 //	int j = 0;
-//	int next[50] = { 0 };
+//	long i = start;
+//	int t = strlen( pattern );
+//	long L = strlen( master );
 //
-//	getnext( pattern, next );
-//	while((i < strlen( master )) && (j < strlen( pattern )))
+//	for(; i < L&&j < t; i++)
 //	{
-//		/* j = -1 表示next[0], 说明失配处在模式串T的第0个字符。所以这里特殊处理，然后令i+1和j+1。*/
-//		if((j == -1) || master[i] == pattern[j])
+//		if(*(master + i) == *(pattern + j))
 //		{
-//			i++;
 //			j++;
 //		}
 //		else
 //		{
-//			j = next[j];
+//			j = 0;
 //		}
 //	}
-//
-//	if(strlen( pattern ) == j)
-//	{
-//		return i - strlen( pattern );
-//	}
-//	else
-//	{
-//		return -1;
-//	}
-//
+//	return i-start;
 //}
+//TODO:此处kmp算法实现不成功
+int next[60];
+
+void get_next( char* t, int next[] )
+{
+	int i = 1;
+	int j = 0;
+	next[0] = -1;
+	while(i<strlen( t ))
+	{
+		if(j == -1 || t[i] == t[j])
+		{
+			i++;
+			j++;
+			if(t[i] != t[j])   //只有前后两个字符不相同时才会按前面的算法来计算next， 即next[i] = j
+				next[i] = j;
+			else
+				next[i] = next[j];        //当前后两个字符相同时，进行向前找，能够减少比较的次数
+		}
+		else
+		{
+			j = next[j];
+		}
+	}
+}
+
+int kmp( char *master, char *pattern ,long start)
+{
+	get_next( pattern, next );
+	long i = start;
+	int j = 0;
+	int len1 = strlen( master );
+	int len2 = strlen( pattern );
+	while(i < len1&&j < len2)
+	{
+		if(j == -1 || master[i] == pattern[j])//第一个字符都不匹配或者字符相等的情况
+		{
+			i++;
+			j++;
+		}
+		else   //i指针不回溯,j指针
+		{
+			//i=i-j+1;
+			j = next[j];
+		}
+	}
+	if(j >= len2)
+		return i - start;
+	else
+		return -1;
+}
