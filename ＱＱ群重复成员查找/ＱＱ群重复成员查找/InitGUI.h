c51e39700fc9e543a03c62cbc6f8e59999f3d5e7
学生李令新 <stullx@qq.com>
2017-07-03 21:23:04 +0800

    初始信息输出封装

---
+++ ＱＱ群重复成员查找/ＱＱ群重复成员查找/InitGUI.h
@@ -0,0 +1,23 @@
+#pragma once
+//初始化界面
+void Init()
+{
+	/*system( "chcp 65001" );*/
+	system( "cls" );
+	printf( "  = = = = = = = = = = = = = = = = =\n" );
+	printf( "\n  QQ群重复成员查找\n" );
+	printf( "\n  数据结构课程设计\n" );
+	printf( "\n  李令新 0121503490118\n" );
+	printf( "\n  指导老师：燕翔\n" );
+	printf( "\n" );
+	printf( "  = = = = = = = = = = = = = = = = =\n" );
+	printf( "\n" );
+
+	printf( "  = = = = = = = = = = = = = = = = =\n" );
+	printf( "\n  测试数据\n" );
+	printf( "\n  正确格式：testA.txt  testB.txt  testC.txt\n" );
+	printf( "\n  错误格式：testERR.txt\n" );
+	printf( "\n" );
+	printf( "  = = = = = = = = = = = = = = = = =\n" );
+	printf( "\n" );
+}
